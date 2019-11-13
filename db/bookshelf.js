56756b5cfa23954b47b9fb162ccde6018cb41b4d
pongthep10 <pongthep10@gmail.com>
2019-11-13 19:24:45 +0700

    rm pass

--- db/bookshelf.js
+++ db/bookshelf.js
@@ -3,7 +3,7 @@ const knex = require('knex')({
     connection: {
       host: 'nsq-db-cluster-instance-1.cfeublxda7lo.ap-southeast-1.rds.amazonaws.com',
       user: 'chatpify_uat',
-      password: 'jfS2wdYY6Am91X2K',
+      password: 'xxxxxxx',
       database: 'chatpify_uat'
     }
   })
